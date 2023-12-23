a3ae6ac91834768d11d02c93170de896c024ea7a
guyettenathanj <138819340+guyettenathanj@users.noreply.github.com>
2023-12-23 14:14:47 -0600

    Update uv_sphere_utils.py

--- utils/uv_sphere_utils.py
+++ utils/uv_sphere_utils.py
@@ -1,1 +1,40 @@
 # code goes here, like how to easily select rings and segments...
+
+def select_poles_of_uv_sphere(sphere):
+    """
+    Selects the top and bottom vertices (poles) of a UV sphere.
+
+    :param sphere: The UV sphere object to modify. Must be of type 'MESH'.
+    """
+    if sphere.type != 'MESH':
+        raise ValueError("Provided object is not a mesh")
+
+    # Switch to edit mode
+    bpy.ops.object.mode_set(mode='EDIT')
+
+    # Get bmesh representation
+    bm = bmesh.from_edit_mesh(sphere.data)
+    bm.verts.ensure_lookup_table()
+
+    # Initialize min and max z values and corresponding vertices
+    max_z = min_z = bm.verts[0].co.z
+    top_vert = bottom_vert = bm.verts[0]
+
+    # Find the top and bottom vertices
+    for vert in bm.verts:
+        if vert.co.z > max_z:
+            max_z = vert.co.z
+            top_vert = vert
+        elif vert.co.z < min_z:
+            min_z = vert.co.z
+            bottom_vert = vert
+
+    # Deselect all vertices
+    bpy.ops.mesh.select_all(action='DESELECT')
+
+    # Select the top and bottom vertices
+    top_vert.select = True
+    bottom_vert.select = True
+
+    # Update the mesh
+    bmesh.update_edit_mesh(sphere.data)
