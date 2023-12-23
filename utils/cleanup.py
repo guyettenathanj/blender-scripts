b62cf48c5571b8cd6b3f4c7bc678ad9cb993001d
guyettenathanj <138819340+guyettenathanj@users.noreply.github.com>
2023-12-23 14:34:28 -0600

    Update cleanup.py

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -1,15 +1,24 @@
 def cleanup_mesh_objects():
+    """
+    Deletes all mesh objects in the current scene.
+    """
     # Switch to object mode
-    bpy.ops.object.mode_set(mode='OBJECT')
-
-    # Deselect all objects
-    bpy.ops.object.select_all(action='DESELECT')
+    if bpy.context.active_object and bpy.context.active_object.mode != 'OBJECT':
+        bpy.ops.object.mode_set(mode='OBJECT')
 
     # Iterate over all objects in the scene
-    for obj in bpy.data.objects:
-        # Check if the object is a mesh
-        if obj.type == 'MESH':
-            # Select the mesh object
-            obj.select_set(True)
-            # Delete the selected object
-            bpy.ops.object.delete() 
+    meshes_to_delete = [obj for obj in bpy.data.objects if obj.type == 'MESH']
+
+    # Check if there are any mesh objects to delete
+    if meshes_to_delete:
+        # Deselect all objects
+        bpy.ops.object.select_all(action='DESELECT')
+
+        # Select mesh objects
+        for mesh in meshes_to_delete:
+            mesh.select_set(True)
+
+        # Delete the selected objects
+        bpy.ops.object.delete() 
+    else:
+        print("No mesh objects found to delete.")
